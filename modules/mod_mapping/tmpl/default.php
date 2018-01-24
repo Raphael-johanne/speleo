a9df9b8ae860a3d935deea280242e914d7b2c44b
Aurelien CARLIER <aureliencarlier123@gmail.com>
2018-01-24 15:41:52 +0100

    Plot relevant icon collors acording to each formula's difficulty

--- modules/mod_mapping/tmpl/default.php
+++ modules/mod_mapping/tmpl/default.php
@@ -17,42 +17,45 @@ $document->addStyleDeclaration($style);
 	function initMap(){
 		
 		var bnds = new google.maps.LatLngBounds();// usefull to center the map among the marker set
-		var iconImage = {url: 'http://speleo.local/speleo/modules/mod_mapping/images/speleoIcon.png'};
 		var infowindow = new google.maps.InfoWindow();
 		var markers = new Array();
+		var map = new google.maps.Map(document.getElementById('map'), {});
+		var icons = {
+			'1' : {icon : 'http://speleo.local/speleo/modules/mod_mapping/images/speleoIcon1.png'},
+			'2' : {icon : 'http://speleo.local/speleo/modules/mod_mapping/images/speleoIcon2.png'},
+			'3' : {icon : 'http://speleo.local/speleo/modules/mod_mapping/images/speleoIcon3.png'}
+		}
 
-		var map = new google.maps.Map(document.getElementById('map'), {
-      		//zoom: 10,
-      		//center: mapCenter,
-    	});
-
+		// main loop creating and placing the markers
 	    <?php foreach( $formules as $formule ): ?>
 	    	placeMarker('<?php echo $formule->name?>',
 	    		Number(<?php echo $formule->lat?>),
 	    		Number(<?php echo $formule->lng?>),
-	    		'<?php echo $formule->short_description?>'
-	    		);
+	    		'<?php echo $formule->short_description?>',
+	    		Number(<?php echo $formule->difficulty?>)
+	    	);
     	<?php endforeach; ?>
 
-
+    	// marker clusterization
 		var markerCluster = new MarkerClusterer(map, markers,
 			{imagePath: 'http://speleo.local/speleo/modules/mod_mapping/images/m'}
 		);
 
+		// adjust the map to the marker set
 		map.setCenter(bnds.getCenter());
         map.fitBounds(bnds);
 
-    	function placeMarker(name, lat, lng, short_description){
+    	function placeMarker(name, lat, lng, short_description, difficulty){
 			var marker = new google.maps.Marker({
 	          	title: name, 
 	          	position: {lat: lat, lng: lng},
 	          	map: map,
-	          	icon: iconImage,
+	          	icon: icons[difficulty].icon,
 	        });
 	        markers.push(marker);
 	        bnds.extend(marker.position); // extends the map bounds
 	        google.maps.event.addListener(marker, 'click', function(){
-		        infowindow.close(); // Close previously opened infowindow
+		        infowindow.close();
 		        infowindow.setContent(infoWindowContent(name, short_description));
 		        infowindow.open(map, marker);
 		    });
