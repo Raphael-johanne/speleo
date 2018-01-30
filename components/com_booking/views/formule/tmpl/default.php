08b49a69ed05aae5df72ba5e5e84cecde1648e68
Raphael Colboc <root@doco6-lamp.devopera>
2018-01-30 11:20:17 +0000

    Add translation for calandar on front

--- components/com_booking/views/formule/tmpl/default.php
+++ components/com_booking/views/formule/tmpl/default.php
@@ -10,7 +10,9 @@
 defined('_JEXEC') or die;
 JHtml::_('jquery.framework');
 JHtml::script(Juri::base() . 'templates/booking/js/lib/jqueryui.min.js');
-JHtml::script(Juri::base() . 'templates/booking/js/booking.js');
+JHtml::script(Juri::base() . 'templates/booking/js/lib/i18n/datepicker-fr.js');
+JHtml::script(Juri::base() . 'templates/booking/js/lib/i18n/datepicker-en-GB.js');
+//JHtml::script(Juri::base() . 'templates/booking/js/booking.js');
 ?>
 
 <?php if ($this->item->lat && $this->item->lng): ?>
@@ -70,7 +72,8 @@ JHtml::script(Juri::base() . 'templates/booking/js/booking.js');
         'booking',
         'overview',
         'errors',
-        <?php echo $this->item->id ?>
+        <?php echo $this->item->id ?>,
+        '<?php echo $this->localeTag ?>'
     );
     booking.loadStep();
 </script>
