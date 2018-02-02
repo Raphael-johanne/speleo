b7ac71e324beb0ec3c70b2b5371ac682c2827900
Raphael Colboc <root@doco6-lamp.devopera>
2018-02-02 14:24:01 +0000

    fix-js-error

--- components/com_booking/views/formule/tmpl/default.php
+++ components/com_booking/views/formule/tmpl/default.php
@@ -15,12 +15,11 @@ JHtml::script(Juri::base() . 'templates/booking/js/lib/i18n/datepicker-en-GB.js'
 JHtml::script(Juri::base() . 'templates/booking/js/booking.js');
 ?>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
 
   <script>
-    $(document).ready(function(){
-      $('.imagesSlider').bxSlider();
+    jQuery(document).ready(function(){
+      jQuery('.imagesSlider').bxSlider();
     });
   </script>
 
@@ -66,7 +65,7 @@ JHtml::script(Juri::base() . 'templates/booking/js/booking.js');
     </li>
 </ul>
 
-<?php if (!empty($this->images)) : ?>
+<?php if (!empty($this->images)) :  ?>
     <div class="imagesSlider">
         <?php foreach($this->images as $image) : ?>
             <div><img src="<?php echo Juri::base() . $image->path ?>"/> </div>
